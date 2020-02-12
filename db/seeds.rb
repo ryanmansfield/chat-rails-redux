43922e4ae5f61ff7b2900b15478a22aa86514c73
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-11 19:42:03 -0600

    Database seeded with users, channels, and messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+puts "All Messages, Users, and Channels have been destroyed, Creating Channels"
+
+user_names = ['Jack', 'Jill']
+channel_names = ['USA', 'React', 'General']
+
+
+
+channels = channel_names.map do |name|
+  Channel.find_or_create_by( name: name)
+end
+
+
+puts "Channels Seeded, Creating Users"
+
+users = user_names.map do |name|
+  User.create(email: "#{name}@gmail.com", password: "123456")
+end
+
+puts "Users Seeded, Creating Messages"
+
+20.times do
+  Message.create! user: users.sample, channel: channels.sample, content: Faker::TvShows::BojackHorseman.quote
+end
+
+puts "DATABASE SEEDED"
