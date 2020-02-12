9335bed1d4ad0e2d154573f55b01a56350d16e0f
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-11 20:19:27 -0600

    Messages index action completed

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::MessagesController < ApplicationController
+  before_action :set_channel
   def index
+    messages = Message.order(created_at: :desc)
+    render json: messages
   end
 
   def create
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.find_by_name(params[:name])
   end
 end
