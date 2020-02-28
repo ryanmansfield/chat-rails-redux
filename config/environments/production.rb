c28a503fb3fbe42ab29342627d2f760dd3d267dd
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-28 00:22:48 -0600

    Initial config for action cable in development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.web_socket_server_url = "wss://rails-redux-chat.herokuapp.com/channels/"
-  config.action_cable.allowed_request_origins = ['https://rails-redux-chat.herokuapp.com', 'http://rails-redux-chat.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -49,6 +47,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+
+  config.web_socket_server_url = "wss://rails-redux-chat.herokuapp.com/channels/"
+  config.action_cable.allowed_request_origins = ['https://rails-redux-chat.herokuapp.com', 'http://rails-redux-chat.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
