3b6252790e4a5ea7688c93717022902febe0cd80
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-28 00:35:53 -0600

    Configured action cable to production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.web_socket_server_url = "wss://rails-redux-chat.herokuapp.com/channels/"
+
+
+  config.web_socket_server_url = "wss://rails-redux-chat.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://rails-redux-chat.herokuapp.com', 'http://rails-redux-chat.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
